55bcafb06ff965b4d59fc8109d438e0b5a353095
Yash Shukla <yashysshukla@gmail.com>
2026-08-18 20:14:51 +0530

    [719] [Time Beats: 57.09%] [Memory Beats: 68.21%] - LeetPush

--- 719-Find-K-th-Smallest-Pair-Distance.cpp
+++ 719-Find-K-th-Smallest-Pair-Distance.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
-        
+        sort(nums.begin(), nums.end()); // kya fayda?
+
+        int l = 0, r = *max_element(nums.begin(), nums.end());
+
+        int n = nums.size();
+        int ans = 0;
+
+        while (l < r) {
+            // log n
+            int m = l + (r - l) / 2;
+            // we will count how many diff before this
+
+            int x = 0;
+            int total = 0;
+
+            for (int y = 1; y < n; y++) {
+                // n
+                // we have to check x-y
+                while (nums[y] - nums[x] > m) {
+                    x++;
+                }
+                total += y - x;
+            }
+            if (total < k) {
+                l = m + 1;
+            } else if (total >= k) {
+                r = m;
+            }
+        }
+
+        return r;
     }
 };
